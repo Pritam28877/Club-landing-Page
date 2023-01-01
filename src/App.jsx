075e83a0b66552107355762b98321bf2cf0fc9ef
Pritam Das <dpritam28577@gmail.com>
2023-01-01 20:53:54 +0530

    ADd the footer in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import About from './components/About'
 import CountdownTimer from './components/CountdownTimer'
+import Footer from './components/Footer'
 function App() {
 
   return (
     <div class='' >
-      <NavBar/>
-      <Hero/>
-      <About/>
-      <CountdownTimer/>
+      <NavBar />
+      <Hero />
+      <About />
+      <CountdownTimer />
+      <Footer />
     </div>
   )
 }
